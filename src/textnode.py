7603df00145d47d2de3733e69ffd4b663bb7c879
pvsq <pv_sqra@protonmail.com>
2024-03-17 19:44:36 +0530

    textnode.py: Add method text_node_to_html_node

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
+from leafnode import LeafNode
+
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -13,4 +15,20 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+    def text_node_to_html_node(self):
+        if self.text_type == "text":
+            return LeafNode(None, self.text)
+        if self.text_type == "bold":
+            return LeafNode("b", self.text)
+        if self.text_type == "italic":
+            return LeafNode("i", self.text)
+        if self.text_type == "code":
+            return LeafNode("code", self.text)
+        if self.text_type == "link":
+            return LeafNode("a", self.text, { "href": self.url })
+        if self.text_type == "image":
+            return LeafNode("img", "", { "src": self.url, "alt": self.text })
+
+        raise ValueError(f"Invalid text type {self.text_type}")
 
