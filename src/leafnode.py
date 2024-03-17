2c4805f8f6fe86292863d5c292ab38d8ff7d8658
pvsq <pv_sqra@protonmail.com>
2024-03-17 16:03:53 +0530

    leafnode.py: Values can be empty strings

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,20 +2,25 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
-        self.value = value
         self.tag = tag
+        self.value = value
         self.props = props
         super().__init__(tag, value, props=props)
 
     def to_html(self):
-        if not self.value:
+        # values are allowed to be empty strings
+        if self.value is None:
             raise ValueError("A value is required for HTML leaf node")
 
         if not self.tag:
             return str(self.value)
 
         html_attrs = super().props_to_html()
-        return f"<{self.tag}{html_attrs}>{self.value.strip(' \n\t')}</{self.tag}>"
+
+        if self.value != "":
+            return f"<{self.tag}{html_attrs}>{self.value.strip(' \n\t')}</{self.tag}>"
+
+        return f"<{self.tag}{html_attrs}>"
 
     def __repr__(self):
         return f"""LeafNode: (
