56f2725bbbbc870e9bc506c65d2a71fb66074bb4
pvsq <pv_sqra@protonmail.com>
2024-03-23 13:23:26 +0530

    test_textnode.py: Use string constants

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -13,32 +13,32 @@ from textnode import (
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_bold)
         self.assertEqual(node, node2)
 
 
     def test_eq_whenDifferentUrl(self):
-        node = TextNode("This is a text node", "bold", "http://localhost:8008")
-        node2 = TextNode("This is a text node", "bold", "http://localhost:8080")
+        node = TextNode("This is a text node", text_type_bold, "http://localhost:8008")
+        node2 = TextNode("This is a text node", text_type_bold, "http://localhost:8080")
         self.assertNotEqual(node, node2)
 
 
     def test_eq_whenDifferentTextType(self):
-        node = TextNode("This is a text node", "bold", "http://localhost:8080")
-        node2 = TextNode("This is a text node", "italic", "http://localhost:8080")
+        node = TextNode("This is a text node", text_type_bold, "http://localhost:8080")
+        node2 = TextNode("This is a text node", text_type_italic, "http://localhost:8080")
         self.assertNotEqual(node, node2)
 
 
     def test_eq_whenDifferentText(self):
-        node = TextNode("This is a text node", "bold", "http://localhost:8080")
-        node2 = TextNode("This is a text mode", "bold", "http://localhost:8080")
+        node = TextNode("This is a text node", text_type_bold, "http://localhost:8080")
+        node2 = TextNode("This is a text mode", text_type_bold, "http://localhost:8080")
         self.assertNotEqual(node, node2)
 
 
     def test_repr(self):
         expected = "TextNode(This is a text node, bold, http://localhost:8080)"
-        node = TextNode("This is a text node", "bold", "http://localhost:8080")
+        node = TextNode("This is a text node", text_type_bold, "http://localhost:8080")
         self.assertEqual(expected, str(node))
 
 
@@ -49,35 +49,35 @@ class TestTextNode(unittest.TestCase):
 
     def test_textNodeToHTMLNode_whenTextTypeIsText(self):
         text = "Raw text. No need to mark up this paragraph."
-        node = TextNode(text, "text")
+        node = TextNode(text, text_type_text)
         expected = text
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
 
     def test_textNodeToHTMLNode_whenTextTypeIsTextAndHasUrl(self):
         text = "Raw text. No need to mark up this paragraph."
-        node = TextNode(text, "text", "https://www.example.com")
+        node = TextNode(text, text_type_text, "https://www.example.com")
         expected = text
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
 
     def test_textNodeToHTMLNode_whenTextTypeIsBold(self):
         text = "This line makes some bold claims"
-        node = TextNode(text, "bold")
+        node = TextNode(text, text_type_bold)
         expected = f"<b>{text}</b>"
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
 
     def test_textNodeToHTMLNode_whenTextTypeIsItalic(self):
         text = "The information contained in this document is strictly confidential."
-        node = TextNode(text, "italic")
+        node = TextNode(text, text_type_italic)
         expected = f"<i>{text}</i>"
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
 
     def test_textNodeToHTMLNode_whenTextTypeIsCode(self):
         text = 'title = "why html development sucks"; print(doc[::-1])'
-        node = TextNode(text, "code")
+        node = TextNode(text, text_type_code)
         expected = f"<code>{text}</code>"
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
@@ -85,7 +85,7 @@ class TestTextNode(unittest.TestCase):
     def test_textNodeToHTMLNode_whenTextTypeIsImage(self):
         alt = "Picture of a valley with a river flowing in the foreground and snow-capped mountains in the distance."
         src = "https://example.com/valley.jpg"
-        node = TextNode(alt, "image", src)
+        node = TextNode(alt, text_type_image, src)
         expected = f'<img src="{src}" alt="{alt}"></img>'
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
@@ -93,7 +93,7 @@ class TestTextNode(unittest.TestCase):
     def test_textNodeToHTMLNode_whenTextTypeIsLink(self):
         anchortext = "Click here to learn more."
         href_link = "https://www.example.com/about"
-        node = TextNode(anchortext, "link", href_link)
+        node = TextNode(anchortext, text_type_link, href_link)
         expected = f'<a href="{href_link}">{anchortext}</a>'
         self.assertEqual(expected, text_node_to_html_node(node).to_html())
 
