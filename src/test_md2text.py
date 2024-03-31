bb4ec09c265ff5300be9f48dfa9050ee6d1b7f85
pvsq <pv_sqra@protonmail.com>
2024-03-31 14:13:30 +0530

    test_md2text.py: Update tests to use constants and add more tests

--- src/test_md2text.py
+++ src/test_md2text.py
@@ -6,30 +6,51 @@ from md2text import (
 )
 
 class TestMd2Text(unittest.TestCase):
+    IMAGES_TEXT = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+    LINKS_TEXT = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    IMAGES_AND_LINKS_TEXT = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://www.example.com) and [another](https://www.example.com/another) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+
+    EXPECTED_IMAGES_LIST = [('image', 'https://i.imgur.com/zjjcJKZ.png'), ('another', 'https://i.imgur.com/dfsdkjfd.png')]
+    EXPECTED_LINKS_LIST = [('link', 'https://www.example.com'), ('another', 'https://www.example.com/another')]
+
     def test_extractMarkdownImages(self):
-        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+        text = TestMd2Text.IMAGES_TEXT
         matches = extract_markdown_images(text)
-        expected = [('image', 'https://i.imgur.com/zjjcJKZ.png'), ('another', 'https://i.imgur.com/dfsdkjfd.png')]
+        expected = TestMd2Text.EXPECTED_IMAGES_LIST
         self.assertEqual(matches, expected)
 
 
     def test_extractMarkdownLinks(self):
-        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        text = TestMd2Text.LINKS_TEXT
         matches = extract_markdown_links(text)
-        expected = [('link', 'https://www.example.com'), ('another', 'https://www.example.com/another')]
+        expected = TestMd2Text.EXPECTED_LINKS_LIST
         self.assertEqual(matches, expected)
 
 
     def test_extractMarkdownImages_withLinksAsInput(self):
-        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        text = TestMd2Text.LINKS_TEXT
         matches = extract_markdown_images(text)
         expected = []
         self.assertEqual(matches, expected)
 
 
     def test_extractMarkdownLinks_withImagesAsInput(self):
-        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+        text = TestMd2Text.IMAGES_TEXT
         matches = extract_markdown_links(text)
         expected = []
+        self.assertEqual(matches, expected)
+
+
+    def test_extractMarkdownImages_withMixedImagesAndLinksInput(self):
+        text = TestMd2Text.IMAGES_AND_LINKS_TEXT
+        matches = extract_markdown_images(text)
+        expected = TestMd2Text.EXPECTED_IMAGES_LIST
+        self.assertEqual(matches, expected)
+
+
+    def test_extractMarkdownLinks_withMixedImagesAndLinksInput(self):
+        text = TestMd2Text.IMAGES_AND_LINKS_TEXT
+        matches = extract_markdown_links(text)
+        expected = TestMd2Text.EXPECTED_LINKS_LIST
         self.assertEqual(matches, expected)
 
