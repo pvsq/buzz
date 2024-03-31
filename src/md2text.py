78c8b8ce7ab1ebc5667abd0538a5d64fb8dd8849
pvsq <pv_sqra@protonmail.com>
2024-03-31 22:26:32 +0530

    md2text: Add split_nodes_image and split_nodes_link and new tests

--- src/md2text.py
+++ src/md2text.py
@@ -1,9 +1,71 @@
 import re
 
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_image,
+    text_type_link,
+)
+
 def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
 
 def extract_markdown_links(text):
     return re.findall(r"[^!]\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text == "":
+            continue
+        matches = extract_markdown_images(node.text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        text = [node.text]
+        for match in matches:
+            split_text = text[0].split(f"![{match[0]}]({match[1]})", 1)
+            new_nodes.append(TextNode(split_text[0], text_type_text))
+            new_nodes.append(TextNode(match[0], text_type_image, match[1]))
+            if split_text[1:]:
+                text = split_text[1:]
+
+        # text must be left with just one element at this point
+        if text and text[0] != "":
+            new_nodes.append(TextNode(text[0], text_type_text))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text == "":
+            continue
+        matches = extract_markdown_links(node.text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        text = [node.text]
+        for match in matches:
+            split_text = text[0].split(f"[{match[0]}]({match[1]})", 1)
+            if split_text[0][-1] == "!":
+                new_nodes.append(TextNode(split_text[0] + f"[{match[0]}]({match[1]})", 
+                                          text_type_text))
+                if split_text[1:]:
+                    text = split_text[1:]
+                continue
+
+            new_nodes.append(TextNode(split_text[0], text_type_text))
+            new_nodes.append(TextNode(match[0], text_type_link, match[1]))
+            if split_text[1:]:
+                text = split_text[1:]
+
+        # text must be left with just one element at this point
+        if text and text[0] != "":
+            new_nodes.append(TextNode(text[0], text_type_text))
+    return new_nodes
 
--- src/test_md2text.py
+++ src/test_md2text.py
@@ -1,8 +1,20 @@
-import unittest, re
+import unittest
 
 from md2text import (
     extract_markdown_images,
-    extract_markdown_links
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+)
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
 )
 
 class TestMd2Text(unittest.TestCase):
@@ -53,4 +65,153 @@ class TestMd2Text(unittest.TestCase):
         matches = extract_markdown_links(text)
         expected = TestMd2Text.EXPECTED_LINKS_LIST
         self.assertEqual(matches, expected)
+
+
+    def test_splitNodesImage_withSingleNodeInput(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode(
+                "second image", text_type_image, "https://i.imgur.com/3elNhQu.png"
+            ),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesLink_withSingleNodeInput(self):
+        node = TextNode(
+            "This is text with a [link](https://www.example.com) and another [second link](https://www.example.com/another)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.example.com"),
+            TextNode(" and another ", text_type_text),
+            TextNode(
+                "second link", text_type_link, "https://www.example.com/another"
+            ),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesImage_withLinksInput(self):
+        node = TextNode(
+            "This is text with a [link](https://www.example.com) and another [second link](https://www.example.com/another)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode(
+                "This is text with a [link](https://www.example.com) and another [second link](https://www.example.com/another)",
+                text_type_text,
+            ),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesLink_withImagesInput(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                text_type_text,
+            ),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesImage_withMultipleNodesInput(self):
+        nodes = [
+            TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)", text_type_text),
+            TextNode("This node is just text, no inline styles here.", text_type_text),
+            TextNode("This is text with an *emphasized block* of words", text_type_italic),
+            TextNode("Some bold text", text_type_bold),
+            TextNode(
+                "More text with more ![images](https://i.imgur.com/zjjcJKZ.png) and some more ![cool images](https://i.imgur.com/3elNhQu.png)",
+                text_type_text,
+            ),
+            TextNode("Text styled `inline` with a `fixed-width` font", text_type_code),
+            TextNode("This is text with a `code block` word", text_type_text),
+        ]
+        new_nodes = split_nodes_image(nodes)
+        expected = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode("second image", text_type_image, "https://i.imgur.com/3elNhQu.png"),
+            TextNode("This node is just text, no inline styles here.", text_type_text),
+            TextNode("This is text with an *emphasized block* of words", text_type_italic),
+            TextNode("Some bold text", text_type_bold),
+            TextNode("More text with more ", text_type_text),
+            TextNode("images", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and some more ", text_type_text),
+            TextNode("cool images", text_type_image, "https://i.imgur.com/3elNhQu.png"),
+            TextNode("Text styled `inline` with a `fixed-width` font", text_type_code),
+            TextNode("This is text with a `code block` word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesLink_withMultipelNodesInput(self):
+        nodes = [
+            TextNode("This is text with a [link](https://www.example.com) and another [second link](https://www.example.com/another)", text_type_text),
+            TextNode("This node is just text, no inline styles here.", text_type_text),
+            TextNode("This is text with an *emphasized block* of words", text_type_italic),
+            TextNode("Some bold text", text_type_bold),
+            TextNode(
+                "More text with more [links](https://i.imgur.com/zjjcJKZ.png) and some more [cool links](https://i.imgur.com/3elNhQu.png)",
+                text_type_text,
+            ),
+            TextNode("Text styled `inline` with a `fixed-width` font", text_type_code),
+            TextNode("This is text with a `code block` word", text_type_text),
+        ]
+        new_nodes = split_nodes_link(nodes)
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.example.com"),
+            TextNode(" and another ", text_type_text),
+            TextNode("second link", text_type_link, "https://www.example.com/another"),
+            TextNode("This node is just text, no inline styles here.", text_type_text),
+            TextNode("This is text with an *emphasized block* of words", text_type_italic),
+            TextNode("Some bold text", text_type_bold),
+            TextNode("More text with more ", text_type_text),
+            TextNode("links", text_type_link, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and some more ", text_type_text),
+            TextNode("cool links", text_type_link, "https://i.imgur.com/3elNhQu.png"),
+            TextNode("Text styled `inline` with a `fixed-width` font", text_type_code),
+            TextNode("This is text with a `code block` word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesImage_withMixedImageAndLinksInput(self):
+        node = TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://i.imgur.com/3elNhQu.png)", text_type_text)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and a [link](https://i.imgur.com/3elNhQu.png)", text_type_text),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+
+    def test_splitNodesLinks_withMixedImageAndLinksInput(self):
+        node = TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://i.imgur.com/3elNhQu.png)", text_type_text)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a ", text_type_text),
+            TextNode("link", text_type_link, "https://i.imgur.com/3elNhQu.png"),
+        ]
+        self.assertEqual(new_nodes, expected)
 
