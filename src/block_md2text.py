2d547896f91481fde3696e00572d0bb70db9791e
pvsq <pv_sqra@protonmail.com>
2024-04-07 12:33:38 +0530

    block_md2text: Add block_to_block_type and tests

--- src/block_md2text.py
+++ src/block_md2text.py
@@ -1,2 +1,65 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unorderedlist = "unordered_list"
+block_type_orderedlist = "ordered_list"
+
+HEADING_RE = r"^#{1,6} "
+CODE_BLOCK = r"```"
+QUOTE_BLOCK = r"^>"
+UNORDERED_LIST_RE = r"^[*-]"
+ORDERED_LIST_RE = r"^[1-9][0-9]*?\."
+
 def markdown_to_blocks(mdtext):
     return mdtext.split("\n\n")
+
+
+def block_to_block_type(mdblock: str):
+    if re.match(HEADING_RE, mdblock):
+        return block_type_heading
+    if __is_block_type(block_type_code, mdblock):
+        return block_type_code
+    if __is_block_type(block_type_quote, mdblock):
+        return block_type_quote
+    if __is_block_type(block_type_unorderedlist, mdblock):
+        return block_type_unorderedlist
+    if __is_block_type(block_type_orderedlist, mdblock):
+        return block_type_orderedlist
+
+    return block_type_paragraph
+
+
+def __is_block_type(block_type, mdblock):
+    if (block_type == block_type_heading
+            or block_type == block_type_paragraph):
+        return False
+
+    lines = mdblock.split("\n")
+    if lines[-1] == "":
+        lines = lines[:-1]
+
+    if block_type == block_type_code:
+        if not (re.match(CODE_BLOCK, lines[0])
+            and re.match(CODE_BLOCK, lines[-1])):
+            return False
+
+    if block_type == block_type_quote:
+        for line in lines:
+            if not re.match(QUOTE_BLOCK, line):
+                return False
+
+    if block_type == block_type_unorderedlist:
+        for line in lines:
+            if not re.match(UNORDERED_LIST_RE, line):
+                return False
+
+    if block_type == block_type_orderedlist:
+        for line in lines:
+            if not re.match(ORDERED_LIST_RE, line):
+                return False
+
+    return True
+
--- src/test_block_md2text.py
+++ src/test_block_md2text.py
@@ -52,3 +52,108 @@ This is the same paragraph on a new line
 * with items"""]
         self.assertEqual(blocks, expected)
 
+
+    def test_blockToBlockType(self):
+        mdtext = """# Heading of The First Level
+
+This is **bolded** paragraph
+
+>Did you know?
+>This block of text is a quote
+>Every line of this block
+>Must start with ">"
+
+>This is not a quote
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+1. An ordered list
+2. starting with the list item
+3. numbered 1.
+4. and ending with
+5. list item number
+6. 5.
+
+* This is a list
+* with items
+
+```
+Vapor
+wave
+Aesthetic
+```
+"""
+        blocks = markdown_to_blocks(mdtext)
+        block_types = []
+        for block in blocks:
+            block_types.append(block_to_block_type(block))
+        expected = [
+            block_type_heading, block_type_paragraph, block_type_quote,
+            block_type_paragraph, block_type_orderedlist,
+            block_type_unorderedlist, block_type_code
+        ]
+        self.assertListEqual(block_types, expected)
+
+
+    def test_blockToBlockType_allHeadingsInput(self):
+        mdtext = """# Heading Level 1
+
+normal paragraph at level 1
+
+## Heading Level 2
+
+### Heading Level 3
+
+1. list item at level 3
+2. list item 2 at level 3
+3. list item 3 at level 3
+
+#### Heading Level 4
+
+##### Heading Level 5
+
+###### Heading Level 6
+
+some more text at the end."""
+        blocks = markdown_to_blocks(mdtext)
+        block_types = []
+        for block in blocks:
+            block_types.append(block_to_block_type(block))
+        expected = [
+            block_type_heading, block_type_paragraph, block_type_heading,
+            block_type_heading, block_type_orderedlist, block_type_heading,
+            block_type_heading, block_type_heading, block_type_paragraph,
+        ]
+        self.assertListEqual(block_types, expected)
+
+
+    def test_blockToBlockType_twoDigitOrderedListItemsInput(self):
+        mdtext = """1. List item
+2. List item
+3. List item
+4. List item
+5. List item
+6. List item
+7. List item
+8. List item
+9. List item
+10. List item
+11. List item
+12. List item
+13. List item
+14. List item
+15. List item
+16. List item
+17. List item
+18. List item
+19. List item
+20. List item
+21. List item
+"""
+        blocks = markdown_to_blocks(mdtext)
+        block_types = []
+        for block in blocks:
+            block_types.append(block_to_block_type(block))
+        expected = [block_type_orderedlist]
+        self.assertListEqual(block_types, expected)
+
